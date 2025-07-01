260cd380dc0eb098eb4fdcd1b7f9a399db103b49
unknown <mehmetcagriekici@gmail.com>
2025-07-01 17:53:01 +0300

    arrow key navigation

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 2. Animate to height auto
    https://www.joshwcomeau.com/snippets/html/interpolate-size/
+
+3. Arrow keys navigation
+   https://stackoverflow.com/questions/29581743/how-do-i-achieve-arrow-key-navigation-between-divs
+
+4. Key codes
+   https://asawicki.info/nosense/doc/devices/keyboard/key_codes.html
--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const faqList = document.getElementById("faq-list");
 const state = {
   errorMessage: "",
   questions: {},
-  isExpanded: false,
+  arrowIndex: undefined, //arrow keys navigation
+  clickIndex: undefined, //mouse click navigation
+  expandedIndex: undefined, //
+  faqPanelsListStore: [],
+  faqToggleIconsListStore: [],
+  faqToggleButtonsListStore: [],
 };
 
 //event listeners
@@ -14,7 +19,10 @@ const state = {
 window.addEventListener("load", init);
 
 //click on the list elements via event delegation
-faqList.addEventListener("click", toggleFaq);
+faqList.addEventListener("click", clickNavigation);
+
+//event listener for the arrow keys keyboard navigation
+document.addEventListener("keydown", arrowNavigation);
 
 //event listeners callback functions
 
@@ -47,70 +55,182 @@ async function init() {
 
   //assign the inner html
   faqList.innerHTML = listInnerHTML;
+
+  //select all the panels and store them inside the state to reuse them on toggle
+  state.faqPanelsListStore = faqList.querySelectorAll(".answer-panel");
+
+  //select all the faq icons and store them inside the state to reuse them on toggle
+  state.faqToggleIconsListStore = faqList.querySelectorAll(
+    ".expand-collapse-icon"
+  );
+
+  //select all the buttons and store them inside the state to reuse them on arrow navigation
+  state.faqToggleButtonsListStore =
+    faqList.querySelectorAll(".question-trigger");
 }
 
 /**
- * function to expand/collapse faq list element answer panel
+ * function to expand/collapse faq list element answer panel with click
  * @param event - click event on the form
  */
-function toggleFaq(event) {
-  //closest togge button
+function clickNavigation(event) {
+  //closest toggle button
   const closestButton = event.target.closest(".question-trigger");
 
-  if (closestButton) {
-    //select the expand/collapse icon
-    const toggleIcon = closestButton.querySelector(".expand-collapse-icon");
+  // reset - close all panels and remove marked classes
+  resetAccordion();
 
+  if (closestButton) {
     //select the parent list element
     const faqItem = closestButton.closest(".faq-item");
 
-    //get the connected panel
-    const answerPanel = faqItem.querySelector(".answer-panel");
+    //update the click index // faq-item-index
+    state.clickIndex = faqItem.id.split("-").at(-1);
 
-    //check if the answerPanel is already expandeda
-    if (answerPanel.classList.contains("faq-expanded")) {
-      //close the current element
-      answerPanel.classList.remove("faq-expanded");
-      answerPanel.classList.add("answer-collapsed");
+    //toggle - expand/collapse
+    togglePanel(state.clickIndex);
+  }
+}
 
-      //update the expanded state to false for all panels
-      state.isExpanded = false;
+/**
+ * function for keyboard navigation on key down
+ * up and down keys
+ * start with item 0 for down key initial press
+ * start with the last item for up key initial press
+ * enter press toggles the current answer panel
+ */
+function arrowNavigation(event) {
+  //key codes
+  const upArrowKeyCode = 38; //moves up
+  const downArrowKeyCode = 40; //moves down
+  const enterKeyCode = 13; //toggles
 
-      //convert the current icon to plus again
-      toggleIcon.setAttribute("src", "./assets/images/icon-plus.svg");
+  // reset - close all panels and remove marked classes
+  resetAccordion();
 
-      return;
-    }
+  //watch for up and down
+  if (event.keyCode === upArrowKeyCode || event.keyCode === downArrowKeyCode) {
+    //arrow keys are used for navigation only
+    event.preventDefault();
 
-    //if the clicked question's panel is not already open and user clicks another question:
-    //if there is an expanded question panel
-    if (state.isExpanded) {
-      //select all the panels
-      const panels = faqList.querySelectorAll(".answer-panel");
-      //select all the icons
-      const toggleIcons = faqList.querySelectorAll(".expand-collapse-icon");
+    //initial click - state.arrowIndex is undefined
+    if (state.arrowIndex === undefined) {
+      //if user presses the up arrow key - start from the last element
+      if (event.keyCode === upArrowKeyCode) {
+        //start from the bottom
+        state.arrowIndex = state.questions.length - 1;
 
-      //close all the panels and turn all the icons to plus
-      for (let i = 0; i < panels.length; i++) {
-        panels[i].classList.remove("faq-expanded");
-        panels[i].classList.add("answer-collapsed");
-        toggleIcons[i].setAttribute("src", "./assets/images/icon-plus.svg");
+        //mark the last element
+        markElement();
+      }
+
+      //if user presses the down arrow key - start from the first element
+      if (event.keyCode === downArrowKeyCode) {
+        //start from the top
+        state.arrowIndex = 0;
+
+        //mark the first element
+        markElement();
+      }
+    } else {
+      //navigation
+      //up - increase index
+      if (event.keyCode === downArrowKeyCode) {
+        //increase the arrow index
+        state.arrowIndex = (state.arrowIndex + 1) % state.questions.length;
+
+        //mark the current element
+        markElement();
+      }
+
+      //down - decrease index
+      if (event.keyCode === upArrowKeyCode) {
+        //decrease the arrow index
+        if (state.arrowIndex === 0) {
+          state.arrowIndex = state.questions.length - 1;
+        } else {
+          state.arrowIndex--;
+        }
+
+        //mark the current element
+        markElement();
       }
     }
+  }
 
-    //open the selected panel
-    answerPanel.classList.remove("answer-collapsed");
-    answerPanel.classList.add("faq-expanded");
-
-    //update the toggle icon src to minus
-    toggleIcon.setAttribute("src", "./assets/images/icon-minus.svg");
-
-    //update expanded state to true
-    state.isExpanded = true;
+  //watch for enter key press
+  if (event.keyCode === enterKeyCode) {
+    //toggle (collapse/expand) marked element - current arrow index
+    togglePanel(state.arrowIndex);
   }
 }
 
 //helper functions
+
+/**
+ * function to toggle an element - expand/collapse panel
+ * @param index - navigation index - either arrowIndex or clickIndex
+ */
+function togglePanel(index) {
+  //if the clicked element is already expanded return void
+  //only resetAccordion works - fake toggle
+  if (index === state.expandedIndex) {
+    return;
+  }
+
+  //select the indexed answer panel and toggle icon - navigated to their related question trigger button
+  const answerPanel = document.getElementById(`faq-answer-${index}`);
+
+  const toggleIcon = document.getElementById(`expand-collapse-icon-${index}`);
+
+  //open the selected panel
+  answerPanel.classList.remove("answer-collapsed");
+  answerPanel.classList.add("faq-expanded");
+
+  //update the toggle icon src to minus
+  toggleIcon.setAttribute("src", "./assets/images/icon-minus.svg");
+
+  //update the state.expandedIndex
+  state.expandedIndex = index;
+}
+
+/**
+ * function to manually hover the indexed element with arrow keys
+ * uses the state.arrowIndex
+ */
+function markElement() {
+  //add marked class to the current button
+  state.faqToggleButtonsListStore[state.arrowIndex].classList.add("faq-marked");
+}
+
+/**
+ * function to reset the accordion at the beginning of each click or key press
+ */
+function resetAccordion() {
+  //remove the marked class from the toggle buttons and expanded class from the panels
+
+  //get all of the buttons
+  const toggleButtons = state.faqToggleButtonsListStore;
+
+  //get all the panels
+  const panels = state.faqPanelsListStore;
+
+  //get all the icons
+  const toggleIcons = state.faqToggleIconsListStore;
+
+  //loop over the buttons
+  for (let i = 0; i < toggleButtons.length; i++) {
+    //remove marked class from all the buttons
+    toggleButtons[i].classList.remove("faq-marked");
+
+    //collpase all the panels
+    panels[i].classList.remove("faq-expanded");
+    panels[i].classList.add("answer-collapsed");
+
+    //swap icon
+    toggleIcons[i].setAttribute("src", "./assets/images/icon-plus.svg");
+  }
+}
 
 /**
  * function to get the app data from the data.json file
@@ -141,10 +261,9 @@ async function getData() {
  * FAQ list item element
  * @param question - current question question text
  * @param answer - current question answer text
- * @param isExpanded - if the answer panel is open or not
  * @param key - unique index
  */
-function QuestionComponent({ question, answer, isExpanded, key }) {
+function QuestionComponent({ question, answer, key }) {
   return `
     <!--FAQ item-->
     <li id="faq-item-${key}" class="faq-item">
